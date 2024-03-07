1d94ee08476a79df178c5c1a9f629ae55444d465
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-03-07 06:30:54 +0600

    h
    d

--- Form.js
+++ Form.js
@@ -6,11 +6,37 @@
 function dataForm(event) {
 
 
-   event.preventDefault();
+    event.preventDefault();
 
     let pName = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+
+    let hobby = document.querySelectorAll('input[name="hobby"]:checked');
+
+    let hobbyValue = [];
+
+    hobby.forEach(
+
+        function (hobby) {
+
+            hobbyValue.push(hobby.Value);
+
+        }
+
+
+
+
+    );
+
+
+
+
+
+
+
+
 
 
 
@@ -19,6 +45,8 @@ function dataForm(event) {
     let output = "Name:" + pName + "\n";
     output += "email:" + email + "\n";
     output += "password:" + password + "\n";
+    output += "Gender:" + gender + "\n";
+    output += "Hobby:" + hobbyValue + "\n";
 
 
     let newWindow = window.open("", '_blank');
