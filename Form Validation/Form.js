af862347bb53b746c49936524975b00172a11f8f
NayamulNirob <158471735+NayamulNirob@users.noreply.github.com>
2024-03-09 01:35:09 +0600

    done

--- Form Validation/Form.js
+++ Form Validation/Form.js
@@ -11,18 +11,131 @@ function dataForm(event) {
     let pName = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-    let gender = document.querySelector('input[name="gender"]:checked').value;
+    let gender = document.querySelector('input[name="gender"]:checked');
     let Subject = document.getElementById('subject').value;
     let DOB = document.getElementById('dob').value;
     let Address = document.getElementById('address').value;
     let hobby = document.querySelectorAll('input[name="hobby"]:checked');
 
 
+
+    // start validation
+
+    if (pName == '') {
+
+        alert("Name Can Not Be Empty");
+        return;
+    }
+    else if (pName.length <= 3) {
+
+        alert("Name Can Not Be less then 4 Charectar");
+        return;
+
+    }
+
+    else { pName }
+
+
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+
+
+
+    if (email == '') {
+
+        alert('Can Not be Blank');
+        return;
+    }
+
+    else if (regEx.test(email)) {
+
+    }
+
+    else {
+        alert("Please Enter a Vlaid Email Address");
+        return;
+    }
+
+
+    if (password == ' ') {
+
+        alert('Can Not be Blank');
+        return;
+    }
+
+    else if (password.length >= 8 && password.length <= 20) {
+
+
+    }
+
+    else {
+        alert('password atleast 8 Charectar and maxmimam 20 charectar');
+        return;
+    }
+
+    if (gender == null) {
+
+        alert('Gender Must be Selected');
+        return;
+    }
+
+
+    else {
+
+        gender
+    }
+
+    if (hobby.length == 0) {
+
+        alert('Atleast one hobby Must be Selected');
+        return;
+    }
+
+
+    else {
+
+        hobby
+    }
+    if (Subject === "Subject..") {
+
+        alert("Subject must be Choosen");
+        return;
+    }
+
+    else { Subject}
+
+
+    if(DOB==""){
+
+        alert('Date and Time must be Selected');
+        return;
+    }
+    else{
+        DOB
+    }
+
+    if(Address==""){
+
+        alert('Addrees must be Fillup');
+        return;
+
+    }
+    else{
+        Address
+    }
+
+
+
+    // end validation
+
+
+
     let hobbyValue = [];
 
     hobby.forEach(
 
-        function(hobby) {
+        function (hobby) {
             hobbyValue.push(hobby.value);
 
         }
@@ -34,8 +147,8 @@ function dataForm(event) {
     let output = "Name:" + pName + "\n";
     output += "email:" + email + "\n";
     output += "password:" + password + "\n";
-    output += "Gender:" + gender + "\n";
-    output += "Hobby:" +hobbyValue+ "\n";
+    output += "Gender:" + gender.value + "\n";
+    output += "Hobby:" + hobbyValue + "\n";
     output += "Subject:" + Subject + "\n";
     output += "DOB:" + DOB + "\n";
     output += "Address:" + Address + "\n";
@@ -62,7 +175,7 @@ function dataForm(event) {
 
 
 
-let regForm = document.getElementById("regForm"); 
- regForm.addEventListener('submit', dataForm);
+let regForm = document.getElementById("regForm");
+regForm.addEventListener('submit', dataForm);
 
 
